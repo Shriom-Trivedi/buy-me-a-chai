a616c781173a6da6a2a3b7e21e837b01e3d3a1ee
Shri Om Trivedi <tshriom@gmail.com>
2022-12-15 00:26:29 +0530

    cancel & thank-you page ui is updated

--- pages/cancel.tsx
+++ pages/cancel.tsx
@@ -1,22 +1,26 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function ThankYou() {
   return (
     <div>
       <Head>
         <title>Buy me a chai</title>
-        <meta name="description" content="buy me a chai" />
-        <link rel="icon" href="/tea-cup5.svg" />
+        <meta name='description' content='buy me a chai' />
+        <link rel='icon' href='/tea-cup5.svg' />
       </Head>
 
-      <main className="max-w-2xl p-4 m-auto flex">
-        <div className="flex-1 p-4">
-          <h1 className="text-3xl font-bold">Sorry you had to cancel</h1>
+      <main className='max-w-2xl p-4 m-auto flex flex-col'>
+        <div className='flex-1 p-4'>
+          <h1 className='text-3xl mb-2 font-bold'>Sorry you had to cancel</h1>
           <p>See you next time</p>
         </div>
 
-        <Link href="/">Home</Link>
+        <Link href='/'>
+          <div className='py-4 ml-4 mt-6 flex rounded justify-center w-[15%] cursor-pointer bg-[#4598b4] text-white hover:opacity-80'>
+            Home
+          </div>
+        </Link>
       </main>
     </div>
   );
--- pages/thank-you.tsx
+++ pages/thank-you.tsx
@@ -1,21 +1,25 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function ThankYou() {
   return (
     <div>
       <Head>
         <title>Thank You</title>
-        <meta name="description" content="Buy me a chai" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='Buy me a chai' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className="max-w-2xl p-4 m-auto flex">
-        <div className="flex-1 p-4">
-          <h1 className="text-3xl font-bold">Thank you!</h1>
+      <main className='max-w-2xl p-4 m-auto flex flex-col'>
+        <div className='flex-1 p-4'>
+          <h1 className='text-3xl mb-2 font-bold'>Thank you!</h1>
           <p>Your donation has been received.</p>
         </div>
-        <Link href="/">Home</Link>
+        <Link href='/'>
+          <div className='py-4 ml-4 mt-6 flex rounded justify-center w-[15%] cursor-pointer bg-[#4598b4] text-white hover:opacity-80'>
+            Home
+          </div>
+        </Link>
       </main>
     </div>
   );
